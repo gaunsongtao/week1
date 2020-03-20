bff3096ae1d4d91c175a0eed14c386fbfbd6b6dc
Your Name <you@example.com>
2020-03-20 13:55:19 +0800

    第一次提交

---
+++ src/main/java/com/bawei/guansongtao20200320/MainActivity.java
@@ -0,0 +1,17 @@
+package com.bawei.guansongtao20200320;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*
+        * 创建成功 分享
+        *
+        * */
+    }
+}
