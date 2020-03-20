20c561ad93f2a80d9a7e23d8d542ed87525f2ed0
Your Name <you@example.com>
2020-03-20 13:58:29 +0800

    第一次提交

--- src/main/java/com/bawei/guansongtao20200320/MainActivity.java
+++ src/main/java/com/bawei/guansongtao20200320/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         /*
         * 创建成功 分享
-        *
-        * */
+        *commit提交
+             * */
     }
 }
